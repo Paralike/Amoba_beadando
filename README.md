846023dbbc98fe8864a29d2351eac3adb438f15f
Paralike <steeadam@gmail.com>
2018-05-01 19:56:02 +0200

    Egymásra lépés megszüntetve játék eleje és vége még szügségeltetik

--- README.md
+++ README.md
@@ -4,9 +4,12 @@ Amoba játék beadandója
 A játék Linux-allat íródott!
 
 change log:
+
 05.01 14:10 actualis elem hozzáava mely figyeli hogy az adott mező kié
 
 05.01 19:30 A pálya már meg tudja mondani hogy hányas strikeban vagy.
 
 05.01 19:35 A if feltétel hozzáadva mely felügyeli hogy na nem a táblára kattintasz ne omoljon össze. 
+
+05.01 19:55 Egymásra lépés hibája megoldva
  
--- gm.cpp
+++ gm.cpp
@@ -9,30 +9,6 @@
 using namespace genv;
 
 void szabalyfigyelo(int, int, std::vector<std::vector<widget*>>,int,int&);
-void tesztfuggveny()
-{
-    std::vector<std::vector<int>> d;
-    for(int i=0; i<5; i++)
-    {
-        std::vector<int >tempg;
-        for(int j=0; j<5; j++)
-        {
-            tempg.push_back(j);
-            //g.push_back(j);
-        }
-        d.push_back(tempg);
-    }
-    d[1][1]=5;
-    std::cout << d[1][1] <<std::endl;
-    for(size_t i=0; i<d.size(); i++)
-    {
-        for(size_t j=0; j<d.size(); j++)
-        {
-            std::cout <<d[j][i]<<" ";
-        }
-        std::cout <<std::endl;
-    }
-}
 
 void gm::jatek()
 {
@@ -67,36 +43,40 @@ void gm::jatek()
                         kivalasztottelemi=i;
                         kivalasztottelemj=j;
                     }
+                    if(w[kivalasztottelemj][kivalasztottelemi]->getallapot()!=0)
+                    {
+                        kivalasztottelemi=-1;
+                        kivalasztottelemj=-1;
+                    }
             /*for(size_t i=0; i<w.size(); i++)
                 if(w[i]->ertekel(ev.pos_x,ev.pos_y))
                     kivalasztottelem=i;*/
             //w[kivalasztottelem]->gombreac([=]() {w[0]->markival(ev);});
-            if(kivalasztottelemi!=-1){
-            w[kivalasztottelemj][kivalasztottelemi]->beallito(1);
-            w[kivalasztottelemj][kivalasztottelemi]->gombreac();
-            if(kivalasztottelemj!=-1&&kivalasztottelemj!=-1)
-                gout << move_to(0,0)<<color(0,0,0)<<box(XX,YY)<<color(255,255,255);
-
-            szabalyfigyelo(kivalasztottelemj,kivalasztottelemi,w,1,counter);
-            std::cout << counter<<std::endl;
-            //Ai
-            do
+            if(kivalasztottelemi!=-1)
             {
-                kivalasztottelemj=rand()%(w.size());
-                kivalasztottelemi=rand()%(w.size());
-            }
-            while(w[kivalasztottelemj][kivalasztottelemi]->getallapot()!=0);
-            //std::cout << kivalasztottelemj<<" "<<kivalasztottelemi<<std::endl;
-            //std::cout << kivalasztottelem<<std::endl;
-            w[kivalasztottelemj][kivalasztottelemi]->beallito(2);
+                w[kivalasztottelemj][kivalasztottelemi]->beallito(1);
+                w[kivalasztottelemj][kivalasztottelemi]->gombreac();
+                    gout << move_to(0,0)<<color(0,0,0)<<box(XX,YY)<<color(255,255,255);
 
-            //szabalyfigyelo(kivalasztottelemj,kivalasztottelemi,w,2,counter);
-            //std::cout << counter<<std::endl;
-            counter=0;
+                szabalyfigyelo(kivalasztottelemj,kivalasztottelemi,w,1,counter);
+                //std::cout << counter<<std::endl;
+
+                //Ai
+                do
+                {
+                    kivalasztottelemj=rand()%(w.size());
+                    kivalasztottelemi=rand()%(w.size());
+                }
+                while(w[kivalasztottelemj][kivalasztottelemi]->getallapot()!=0);
+                //std::cout << kivalasztottelemj<<" "<<kivalasztottelemi<<std::endl;
+                //std::cout << kivalasztottelem<<std::endl;
+                w[kivalasztottelemj][kivalasztottelemi]->beallito(2);
+                szabalyfigyelo(kivalasztottelemj,kivalasztottelemi,w,2,counter);
+                //std::cout << counter<<std::endl;
+                counter=0;
             }
         }
 
-        //játékmester
 
 
         if(kivalasztottelemj!=-1)
@@ -114,7 +94,6 @@ void gm::jatek()
 void szabalyfigyelo(int aktualislepes_j,int aktualislepes_i,std::vector<std::vector<widget*>> w, int szemely,int &counter)
 {
     counter=0;
-
     //függőleges vonal
     int maxi=0;
     for(int i=1; i<w.size(); i++)
@@ -224,7 +203,7 @@ void szabalyfigyelo(int aktualislepes_j,int aktualislepes_i,std::vector<std::vec
                 maxi++;
             if(counter<maxi)
                 counter=maxi;
-                //std::cout << maxi<<std::endl;
+            //std::cout << maxi<<std::endl;
             if(w[aktualisj-1][aktualisi+1]->getallapot()!=szemely)
                 maxi=0;
             //std::cout << aktualisj <<" "<<aktualisi<<std::endl;
