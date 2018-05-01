131f69a0274c66aeccef6cad1d8ec12e754cd280
Paralike <steeadam@gmail.com>
2018-05-01 13:07:04 +0200

    skeleton is running but it's not showing anyhing

--- tools.cpp
+++ tools.cpp
@@ -8,14 +8,16 @@
 
 tools::tools()
 {
-    for(int i=1; i<16;i++)
+    c = new checkbox(5,10,30,30);
+    w.push_back(c);
+    /*for(int i=1; i<16;i++)
     {
         for(int j=1; j<16;i++)
         {
             c = new checkbox(5*j,10*i,30,30);
             w.push_back(c);
         }
-    }
+    }*/
 
 
 }
